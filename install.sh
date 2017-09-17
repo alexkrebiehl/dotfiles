bdec715426fb410ac10eeebed78064a4f1fbf7c1
Alex Krebiehl <alex.krebiehl@gmail.com>
2017-09-17 13:39:01 -0400

    Added bash prompt

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s $CURRENT/.vim ~/.vim
 ln -s $CURRENT/.vimrc ~/.vimrc
+
+echo ". $CURRENT/.bash_prompt" >> ~/.bashrc
+source ~/.bashrc
