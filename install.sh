811e9dce3fd606610091138f5ab66e4857c88e27
Alex Krebiehl <alex.krebiehl@gmail.com>
2017-09-23 09:44:59 -0400

    Added git submodule setup to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $CURRENT
+
+git submodule init
+git submodule update
 
 ln -s $CURRENT/.vim ~/.vim
 ln -s $CURRENT/.vimrc ~/.vimrc
