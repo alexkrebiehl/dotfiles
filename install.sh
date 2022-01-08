172eadb09110597ddbb3886805464e749c70a74a
alex <alex.krebiehl@gmail.com>
2022-01-08 06:08:25 +0000

    changing script directory discovery

--- install.sh
+++ install.sh
@@ -4,8 +4,16 @@ INPUT="$1"
 DEFAULT_INSTALL_DIR="$HOME"
 INSTALL_DIR=${INPUT:-"$DEFAULT_INSTALL_DIR"}
 
-CURRENT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $CURRENT
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+    SOURCE=$(readlink "$SOURCE")
+      [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+cd "$DIR"
+
+echo "Installing dotfiles from $DIR"
 
 git submodule init
 git submodule update
@@ -18,25 +26,25 @@ git submodule update
 curl -L git.io/antigen > "$INSTALL_DIR/.antigen.zsh"
 
 mv "$INSTALL_DIR/.zshrc" "$INSTALL_DIR/.zshrc.bak"
-ln -s "$CURRENT/.zshrc" "$INSTALL_DIR/.zshrc"
+ln -s "$DIR/.zshrc" "$INSTALL_DIR/.zshrc"
 
 mv "$INSTALL_DIR/.p10k.zsh" "$INSTALL_DIR/.p10k.zsh.bak"
-ln -s "$CURRENT/.p10k.zsh" "$INSTALL_DIR/.p10k.zsh"
+ln -s "$DIR/.p10k.zsh" "$DIR/.p10k.zsh"
 
 ########################
 # vim setup
 ########################
 mv "$INSTALL_DIR/.vim" "$INSTALL_DIR/.vim.bak"
-ln -s "$CURRENT/.vim" "$INSTALL_DIR/.vim"
+ln -s "$DIR/.vim" "$INSTALL_DIR/.vim"
 
 mv "$INSTALL_DIR/.vimrc" "$INSTALL_DIR/.vimrc.bak"
-ln -s "$CURRENT/.vimrc" "$INSTALL_DIR/.vimrc"
+ln -s "$DIR/.vimrc" "$INSTALL_DIR/.vimrc"
 
 ########################
 # tmux setup
 ########################
 mv "$INSTALL_DIR/.tmux.conf" "$INSTALL_DIR/.tmux.conf.bak"
-ln -s "$CURRENT/.tmux.conf" "$INSTALL_DIR/.tmux.conf"
+ln -s "$DIR/.tmux.conf" "$INSTALL_DIR/.tmux.conf"
 
 ########################
 # wrap up
